230bfcc9aeaa721fb5b4ca9c698aaddb761f40a2
VanadeysHaven <Cooltimmetje@me.com>
2021-07-11 17:48:47 +0200

    more config

--- src/main/java/me/VanadeysHaven/TimmyCore/Listeners/ServerPingListener.java
+++ src/main/java/me/VanadeysHaven/TimmyCore/Listeners/ServerPingListener.java
@@ -1,50 +1,38 @@
 package me.VanadeysHaven.TimmyCore.Listeners;
 
 import lombok.Getter;
+import me.VanadeysHaven.TimmyCore.Main;
 import me.VanadeysHaven.TimmyCore.Utilities.StringUtilities;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.material.PressurePlate;
 
 import java.util.List;
 import java.util.Random;
 
 public final class ServerPingListener implements Listener {
 
-    private static final String motd = "&8[&61.17&8] » &3&lN&3euro&a&lC&araft\n&3&lN&a&lC &8» &b&o";
+    private static Random random =  new Random();
+    private static String motdFormat;
+    private static String name;
+    private static String shortName;
+    private static List<String> motds;
 
-    private enum Messages {
-
-        POWERED_BY("Powered by Sparked Host - sparkedhost.com"),
-        STIMMING("Stimming, together."),
-        PUPPIES("PUPPIES!!!"),
-        BLOCKY("Mmm... Blocky..."),
-        MEMES("Now with memes!"),
-        EST("Est. 1986"),
-        CHEESE("Now with cheese!"),
-        BEES("BEES!"),
-        HI("Hello!"),
-        FIRE("In case of fire; yell at Timmy.");
-
-        @Getter private String message;
-
-        Messages(String message){
-            this.message = message;
-        }
-
-
-        private static final List<Messages> VALUES = List.of(values());
-        private static final int SIZE = VALUES.size();
-        private static final Random RANDOM = new Random();
-
-        public static Messages random()  {
-            return VALUES.get(RANDOM.nextInt(SIZE));
-        }
+    public ServerPingListener(){
+        FileConfiguration config = Main.getPlugin().getConfig();
+        motdFormat = config.getString("motd.format");
+        name = config.getString("server.name");
+        shortName = config.getString("server.shortName");
+        motds = config.getStringList("motd.motds");
     }
 
     @EventHandler
     public void onPing(ServerListPingEvent event){
-        event.setMotd(StringUtilities.colorify(motd + Messages.random().getMessage()));
+        String format = motdFormat.replace("$name", name).replace("$shortName", shortName);
+        format = format.replace("$motd", motds.get(random.nextInt(motds.size() - 1)));
+        event.setMotd(StringUtilities.colorify(format));
     }
 
 }
